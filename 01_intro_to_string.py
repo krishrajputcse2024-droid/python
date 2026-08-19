9aaab561a23a9781218e2821e2f45886c9d4159f
krishrajputcse2024-droid <krish.rajput.cse.2024@miet.ac.in>
2026-08-19 11:14:43 +0530

    Add files via upload
    string

---
+++ 01_intro_to_string.py
@@ -0,0 +1,19 @@
+name = "krish"
+
+"""nameshort = name[0:3]
+
+print(nameshort)
+character1 = name[2]
+print(character1)"""
+"""""print(name[-4 : -1])
+print(name[1:4])
+print(name[ : 4])
+print(name[1:])"""
+
+name = "krish rajput"
+capit = name.capitalize()
+print(capit)
+""""print(len(name))
+print(name[1:4:2])
+count = name.count("t")
+print(count)"""""
